2bcd840160aa9a3142ad71192470f2438fc23378
José Luis Cebrián <jlcebrian@gmail.com>
2023-10-11 23:56:03 +0200

    Switched Windows native version to 32 bits EXE

--- build.bat
+++ build.bat
@@ -51,24 +51,60 @@ REM -----------------------------------------------------------------
 REM  Windows x64 executables compiled with Visual Studio
 REM -----------------------------------------------------------------
 :WINDOWS
+SETLOCAL
+SET WIN=win32
+
 WHERE cl >NUL 2>NUL
 IF %ERRORLEVEL% NEQ 0 call devenv.bat
-
-SETLOCAL
 
 SET VERSION=/DVERSION=%VERSION%
 SET TRACE=/DTRACE_ON=1
 SET WARNINGS=/W4 /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE 
-SET TOOLOPTS=/Iinclude-tools /Ilib-win64/libpng
-SET OPTS=/Fo:obj\ /Fd:out\ /nologo /MT /Ilib-win64\libpng /Ilib-win64\sdl\include /Iinclude /D_STDCLIB /DNO_CACHE /DHAS_CLIPBOARD /DHAS_FULLSCREEN /DHAS_VIRTUALFILESYSTEM
+SET TOOLOPTS=/Iinclude-tools /Ilib/libpng
+SET OPTS=/Fo:obj\ /Fd:out\ /nologo /MT /Ilib\libpng /Ilib\sdl2\include /Iinclude /D_STDCLIB /DNO_CACHE /DHAS_CLIPBOARD /DHAS_FULLSCREEN /DHAS_VIRTUALFILESYSTEM
 set OPTIM=/Zi /GR- /EHsc /MP8 /FC /GL
 SET LINK=
 
 IF "%2"=="Release" SET OPTIM=/O2 /GR- /EHsc /MP8 /GL
 IF "%2"=="Release" SET LINK=/DEBUG:NONE
 
-SET LIBS=lib-win64\sdl2\lib\SDL2-static.lib lib-win64\sdl2\lib\SDL2main.lib user32.lib gdi32.lib comdlg32.lib ole32.lib shell32.lib setupapi.lib winmm.lib imm32.lib version.lib advapi32.lib oleaut32.lib
-SET TOOLLIBS=lib-win64\libpng\libpng16.lib lib-win64\zlib\zlib.lib
+SET LIBS=lib\sdl2\%WIN%\SDL2-static.lib lib\sdl2\%WIN%\SDL2main.lib user32.lib gdi32.lib comdlg32.lib ole32.lib shell32.lib setupapi.lib winmm.lib imm32.lib version.lib advapi32.lib oleaut32.lib
+SET TOOLLIBS=lib\libpng\%WIN%\libpng16.lib lib\zlib\%WIN%\zlib.lib
+
+echo ---- Compiling PLAYER
+cl %VERSION% %OPTS% %OPTIM% /Fe:out\player.exe /DDEBUG_ALLOCS ^
+	src-common\ddb.cpp ^
+	src-common\os_char.cpp ^
+	src-common\ddb_data.cpp ^
+	src-common\ddb_dump.cpp ^
+	src-common\ddb_inp.cpp ^
+	src-common\ddb_pal.cpp ^
+	src-common\ddb_play.cpp ^
+	src-common\ddb_run.cpp ^
+	src-common\ddb_vid.cpp ^
+	src-common\ddb_scr.cpp ^
+	src-common\dmg_cach.cpp ^
+	src-common\dmg_imgc.cpp ^
+	src-common\dmg_imgp.cpp ^
+	src-common\dmg_rlec.cpp ^
+	src-common\dmg_univ.cpp ^
+	src-common\dmg_cga.cpp ^
+	src-common\dmg_ega.cpp ^
+	src-common\dmg.cpp ^
+	src-common\dim.cpp ^
+	src-common\dim_adf.cpp ^
+	src-common\dim_cpc.cpp ^
+	src-common\dim_fat.cpp ^
+	src-common\os_file.cpp ^
+	src-common\os_lib.cpp ^
+	src-common\os_mem.cpp ^
+	src-common\scrfile.cpp ^
+	src-sdl\player.cpp ^
+	src-sdl\video.cpp ^
+	src-windows\error.cpp ^
+	src-windows\files.cpp ^
+	%LIBS% lib\ddb.res /link %LINK% /SUBSYSTEM:WINDOWS
+IF %ERRORLEVEL% NEQ 0 GOTO :EOF
 
 echo ---- Compiling DC
 cl %VERSION% %OPTS% %TOOLOPTS% %OPTIM% /Fe:out\dc.exe ^
@@ -144,41 +180,6 @@ cl %VERSION% %OPTS% %TOOLOPTS% %OPTIM% /Fe:out\dmg.exe ^
 	%TOOLLIBS% /link %LINK% /SUBSYSTEM:CONSOLE
 IF %ERRORLEVEL% NEQ 0 GOTO :EOF
 
-echo ---- Compiling PLAYER
-cl %VERSION% %OPTS% %OPTIM% /Fe:out\player.exe  /DDEBUG_ALLOCS ^
-	src-common\ddb.cpp ^
-	src-common\os_char.cpp ^
-	src-common\ddb_data.cpp ^
-	src-common\ddb_dump.cpp ^
-	src-common\ddb_inp.cpp ^
-	src-common\ddb_pal.cpp ^
-	src-common\ddb_play.cpp ^
-	src-common\ddb_run.cpp ^
-	src-common\ddb_vid.cpp ^
-	src-common\ddb_scr.cpp ^
-	src-common\dmg_cach.cpp ^
-	src-common\dmg_imgc.cpp ^
-	src-common\dmg_imgp.cpp ^
-	src-common\dmg_rlec.cpp ^
-	src-common\dmg_univ.cpp ^
-	src-common\dmg_cga.cpp ^
-	src-common\dmg_ega.cpp ^
-	src-common\dmg.cpp ^
-	src-common\dim.cpp ^
-	src-common\dim_adf.cpp ^
-	src-common\dim_cpc.cpp ^
-	src-common\dim_fat.cpp ^
-	src-common\os_file.cpp ^
-	src-common\os_lib.cpp ^
-	src-common\os_mem.cpp ^
-	src-common\scrfile.cpp ^
-	src-sdl\player.cpp ^
-	src-sdl\video.cpp ^
-	src-windows\error.cpp ^
-	src-windows\files.cpp ^
-	%LIBS% lib-win64\ddb.res /link %LINK% /SUBSYSTEM:WINDOWS
-IF %ERRORLEVEL% NEQ 0 GOTO :EOF
-
 echo ---- Compiling DDB
 cl %VERSION% %OPTS% %OPTIM% %TRACE% /Fe:out\ddb.exe ^
 	src-common\ddb.cpp ^
@@ -211,7 +212,7 @@ cl %VERSION% %OPTS% %OPTIM% %TRACE% /Fe:out\ddb.exe ^
 	src-sdl\video.cpp ^
 	src-windows\error.cpp ^
 	src-windows\files.cpp ^
-	%LIBS% lib-win64\ddb.res /link %LINK% /SUBSYSTEM:CONSOLE
+	%LIBS% lib\ddb.res /link %LINK% /SUBSYSTEM:CONSOLE
 IF %ERRORLEVEL% NEQ 0 GOTO :EOF
 
 :EOF
